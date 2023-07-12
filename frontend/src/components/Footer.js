88f8b4e428039e5f913dff1dc06a597a489db57d
Lianne <liannegahan@gmail.com>
2023-07-12 12:20:42 +0100

    - Added details about website creation to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,16 @@
 
-import { Box, Button, Toolbar } from '@mui/material';
+import { Box, Button, Typography, Toolbar } from '@mui/material';
 
 const Footer = () => {
 
     return (
         <>
             <Toolbar disableGutters  >
+            <Typography align="center" variant="body1" margin="1em">
+                    Website built and maintained by Dr Lianne Gahan
+                    </Typography>
                 <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: "flex-end", }}>
+                    
                     <Button sx={{ color: "black" }} size="small" href="https://www.hhu.de/impressum" variant="outline" color="primary">Impressum</Button>
                     <Button sx={{ color: "black" }} size="small" href="https://www.hhu.de/datenschutzerklaerung" variant="outline" color="primary">Data Privacy</Button>
                 </Box>
